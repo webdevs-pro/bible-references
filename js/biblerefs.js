c8f272bf7091f12fb3bea0885bff9ccc02c4d0b5
AlexSoundmaster <shock_31337@ukr.net>
2020-05-07 12:52:58 +0300

    0.7.6

--- js/biblerefs.js
+++ js/biblerefs.js
@@ -5,34 +5,44 @@ jQuery(document).ready(function($){
 	var verses_query_string = '';
 	// open popup
 	$('span.bg_data_title').click(function(){
-		$('.bible_loading_overlay').addClass('bible_loading_overlay_vivsible');
-
+		
 		// get verses query string
 		verses_query_string = $(this).attr('data-title');
-		console.log(verses_query_string);
 
 		// open popup
 		$('.bible_references').addClass('br_popup_visible');
+		
+		// show loading overlay
+		$('.bible_loading_overlay').addClass('bible_loading_overlay_vivsible');
+		
 		$('#bible_books').val(getQueryStringParameter(verses_query_string, 'lang'));	
 		
+		// get verses with AJAX call and append
 		setTimeout(function(){
-			// print verses 
 			$('.bible_references_popup_verses').html(getBibleVerses(verses_query_string));			
 		}, 100);
-
 
 	});
 
 
 
+	
+	
 
 	// select translation
 	$('#bible_books').on('change', function(e){
-		console.log($(this).val());
+		
+		// show loading overlay
+		$('.bible_loading_overlay').addClass('bible_loading_overlay_vivsible');
+		
+		// prepare query string
 		var new_verses_query_string = updateQueryStringParameter(verses_query_string, 'lang', $(this).val());
-
-		// print verses
-		$('.bible_references_popup_verses').html(getBibleVerses(new_verses_query_string));
+		
+		// get verses with AJAX call and apeend
+		setTimeout(function(){
+			$('.bible_references_popup_verses').html(getBibleVerses(new_verses_query_string));
+		}, 100);
+		
 	});
 
 
@@ -42,6 +52,8 @@ jQuery(document).ready(function($){
 
 	// close popup
 	$('.bible_references,.bible_references_popup_close').click(function(e){
+		
+		// not our case
 		if(e.target != this) return;
 
 		$('.bible_references').removeClass('br_popup_visible');
@@ -57,7 +69,7 @@ jQuery(document).ready(function($){
 
 	// get bible verses (AJAX)
 	function getBibleVerses(queryString) {
-		$('.bible_loading_overlay').addClass('bible_loading_overlay_vivsible');
+		
 		var return_verses = '';
 		$.ajax({
 			type: 'GET',
@@ -78,7 +90,11 @@ jQuery(document).ready(function($){
 			}
 			
 		}); 
+		
+		// hide loading overlay
 		$('.bible_loading_overlay').removeClass('bible_loading_overlay_vivsible');
+		
+		// return verses
 		return return_verses;
 		
 	}	
@@ -93,10 +109,10 @@ jQuery(document).ready(function($){
 		var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
 		var separator = uri.indexOf('?') !== -1 ? "&" : "?";
 		if (uri.match(re)) {
-		return uri.replace(re, '$1' + key + "=" + value + '$2');
+			return uri.replace(re, '$1' + key + "=" + value + '$2');
 		}
 		else {
-		return uri + separator + key + "=" + value;
+			return uri + separator + key + "=" + value;
 		}
 	}
 
@@ -116,24 +132,6 @@ jQuery(document).ready(function($){
 	}
   
  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
